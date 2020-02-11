33c81eb7d623103ad8053d410c4c4d4fd56b0b4e
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-02-11 14:17:49 +0530

    Update Home Page with calender

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import AppNavigator from './navigation/AppNavigation';
 import { loadReCaptcha } from 'react-recaptcha-google'
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import { Card } from 'react-native-paper';
 
 export default class App extends React.Component {
+  // const [dataLoaded, setDataLoaded] = useState(false);
+  // if (!dataLoaded) {
+  //   return (
+  //     <AppLoading
+  //     startAsync = {fetchFonts}
+  //     onFinish = {() => setDataLoaded(true)}
+  //     />
+  //   )
+  // }
   render() {
     return (
       <AppNavigator />
--- screens/Home.js
+++ screens/Home.js
@@ -1,56 +1,108 @@
-import * as React from 'react';
-import { Text, View, Button, StyleSheet, AsyncStorage } from 'react-native';
-import Constants from 'expo-constants';
+import React, {Component} from 'react';
+import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Agenda} from 'react-native-calendars';
 
-// or any pure javascript modules available in npm
-import { Card, TextInput } from 'react-native-paper';
+export default class AgendaScreen extends Component {
+  constructor(props) {
+    super(props);
 
-export default class Home extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
-  constructor() {
-    super();
     this.state = {
-      name: '',
-      emailAdress: ''
+      items: {}
     };
-    this._bootstrap();
   }
 
-  _bootstrap = async () => {
-    const firstName = await AsyncStorage.getItem('firstName');
-    this.setState(
-      {
-        name: firstName
-      }
-    );
-
-  }
   render() {
     return (
-      <View style={styles.container}>
-        <Card>
-          <Text> {this.state.name}</Text>
-        </Card>
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2017-05-16'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#43515c'},
+        //    '2017-05-09': {textColor: '#43515c'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+        // hideExtraDays={false}
+      />
+    );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity 
+        style={[styles.item, {height: item.height}]} 
+        onPress={() => Alert.alert(item.name)}
+      >
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
       </View>
     );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  item: {
+    backgroundColor: 'white',
     flex: 1,
-    justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
 });
